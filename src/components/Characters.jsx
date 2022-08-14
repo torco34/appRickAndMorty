d0ba96bc85031407268444e186cb23c177d8c03e
Torcoroma <torcoromaarias@gmail.com>
2022-08-14 11:58:24 -0500

    Agregado Api, axios

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import { CharactersContext } from "../context/characteContext";
 function Characters() {
+  const test = useContext(CharactersContext);
   return (
     <div className="row">
       <div className="col-4">
         <h2 className=" alert alert-danger">Nome</h2>
-        <p>imagen</p>
+        <p>{test}</p>
       </div>
     </div>
   );
--- src/context/characteContext.jsx
+++ src/context/characteContext.jsx
@@ -1,10 +1,20 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
+import Axios from "axios";
 
 export const CharactersContext = createContext();
 
 export const CharactersContextProvider = ({ children }) => {
+  const [character, setCharacters] = useState([]);
+  useEffect(() => {
+    Axios.get("https://rickandmortyapi.com/api/character/").then((response) => {
+      if (response.status == 200) {
+        console.log(response.data);
+        setCharacters(response.data);
+      }
+    });
+  }, []);
   return (
-    <CharactersContext.Provider value={"Rick and Morty"}>
+    <CharactersContext.Provider value={{ character }}>
       {children}
     </CharactersContext.Provider>
   );
